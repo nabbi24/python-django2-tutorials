c1c0ff9ad8e1f0225ed6ef717b242c6e963d28e2
harapan1231 <senang.harapan@gmail.com>
2017-12-11 12:44:47 +0900

    Adjust locale

--- settings.py
+++ settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 #mod: LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ja-jp'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Japan'
 
 USE_I18N = True
 
