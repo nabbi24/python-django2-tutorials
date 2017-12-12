7a4b842a25212cfde9ed284b2198f19fa59960d4
harapan1231 <senang.harapan@gmail.com>
2017-12-12 10:19:02 +0900

    Add a loarders config

--- loaders/models.py
+++ loaders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class CommissionForm(models.Model):
+    form_id = models.CharField(max_length=8, default='')
+    pub_date = models.DateTimeField('date published', default=None)
+    
+    def __str__(self):
+        return self.form_id
+
+class Agency(models.Model):
+    commission_form = models.ForeignKey(
+        CommissionForm,
+        on_delete=models.CASCADE
+    )
+    agency_id = models.CharField(max_length=12, default='')
+    areas = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.agency_id
+
