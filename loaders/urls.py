60a352519512f07f0549b9b7de66cfe69df149c7
harapan1231 <senang.harapan@gmail.com>
2017-12-12 16:08:49 +0900

    Add namespace to loaders' urls

--- loaders/urls.py
+++ loaders/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'loaders'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:form_id>/', views.detail, name='detail'),
