2bffb13b8637a2272f18fae64e7ddd22f1ae0417
harapan1231 <senang.harapan@gmail.com>
2017-12-12 13:10:30 +0900

    Add views

--- loaders/urls.py
+++ loaders/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:form_id>/', views.detail, name='detail'),
+    path('<int:form_id>/results/', views.results, name='results'),
+    path('<str:form_id>/load/', views.load, name='load'),
 ]
 
--- loaders/views.py
+++ loaders/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import CommissionForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    fs = CommissionForm.objects.order_by('form_id')[:5]
+    output = ', '.join([f.form_id for f in fs])
+    return HttpResponse(output)
 
+def detail(request, form_id):
+    return HttpResponse("You're looking at form %s." % form_id)
+
+def results(request, form_id):
+    response = "You're looking at the results of form %s."
+    return HttpResponse(response % form_id)
+
+def load(request, form_id):
+    return HttpResponse("You're loading a form %s." % form_id)
