ca920143826f6e715efefc6be4bb2ee1d25c6940
harapan1231 <senang.harapan@gmail.com>
2017-12-13 11:03:26 +0900

    Implement a loading function

--- loaders/views.py
+++ loaders/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import CommissionForm
+from .models import CommissionForm, Agency
 
 def index(request):
     fs = CommissionForm.objects.order_by('form_id')[:5]
@@ -21,4 +22,16 @@ def results(request, form_id):
     return HttpResponse(response % form_id)
 
 def load(request, form_id):
-    return HttpResponse("You're loading a form %s." % form_id)
+    form = get_object_or_404(CommissionForm, pk=form_id)
+    try:
+        selected_agency = form.agency_set.get(pk=request.POST['agency'])
+    except (KeyError, Agency.DoesNotExist):
+        return render(request, 'loaders/detail.html', {
+            'form': form,
+            'error_message': "You didn't select a valid agency.",
+        })
+    else:
+        selected_agency.area += 1
+        selected_agency.save()
+        
+        return HttpResponseRedirect(reverse('loaders:results', args=(form.id,)))
