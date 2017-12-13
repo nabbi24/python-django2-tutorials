99d9286dab8586957bc86054e62477c711aeea15
harapan1231 <senang.harapan@gmail.com>
2017-12-13 15:07:28 +0900

    Add comments in test codes

--- loaders/tests.py
+++ loaders/tests.py
@@ -11,11 +11,17 @@ from .models import CommissionForm
 class CommissionFormTests(TestCase):
     
     def test_was_published_recently_with_recent_form(self):
+        """
+        was_published_recently returns True for forms whose pub_date is within the last day.
+        """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_form = CommissionForm(pub_date=time)
         self.assertIs(recent_form.was_published_recently(), True)
 
     def test_was_published_recently_with_past_form(self):
+        """
+        was_published_recently returns False for forms whose pub_date is older than 1 day.
+        """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_form = CommissionForm(pub_date=time)
         self.assertIs(old_form.was_published_recently(), False)
