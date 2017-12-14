b159c977db0db602844de58216233013f044fff5
harapan1231 <senang.harapan@gmail.com>
2017-12-14 11:38:58 +0900

    Replace admin.StackedInline with admin.TabuluarInline to let CommissionForm admin page more slim

--- loaders/admin.py
+++ loaders/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import CommissionForm, Agency
 
-class AgencyInline(admin.StackedInline):
+class AgencyInline(admin.TabularInline):
     model = Agency
     extra = 3
 
