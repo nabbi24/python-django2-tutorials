43dcee0f8e0c1984adc5dafb602c33f7453a0b05
harapan1231 <senang.harapan@gmail.com>
2017-12-14 11:35:26 +0900

    Add relationship between CommissionForm and Agency in the former admin page

--- loaders/admin.py
+++ loaders/admin.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import CommissionForm, Agency
 
+class AgencyInline(admin.StackedInline):
+    model = Agency
+    extra = 3
+
 class CommissionFormAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields': ['form_id']}),
-        ('Date information',    {'fields': ['pub_date']}),
+        ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [AgencyInline]
 
 admin.site.register(CommissionForm, CommissionFormAdmin)
-admin.site.register(Agency)
